8ebbaff68e531e040c4d0453193fd47a98a744db
MarianRoja <marianroja1@gmail.com>
2020-07-30 22:36:15 -0300

    se agregaron 3 funciones

--- supermercado.py
+++ supermercado.py
@@ -1,4 +1,16 @@
 class Producto:
-	def __init(self, nombre,precio):
+	def __init(self, nombre,precio, primera_necesidad):
 		self.__nombre+nombre
 		self.__precio+precio
+		self.__primera_necesidad+primera_necesidad
+
+	def get_nombre(self):
+		return self.__nombre
+	def get primera_necesidad(self):
+		return self.__primera_necesidad
+	def precio(self):
+		if primera_necesidad():
+			return self.__precio*0,9
+		else:
+			return self.__precio
+	
